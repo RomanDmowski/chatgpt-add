9297eadb2fbc1bc42fe0beb96af1a8cfbab10514
RomanDmowski <roman.dmowski@outlook.com>
2025-02-24 21:59:38 +0100

    Rename app

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -32,9 +32,9 @@
 <aside class="menu main-menu" class:pinned={$pinMainMenu} use:clickOutside={() => { $pinMainMenu = false }}>
   <div class="menu-expanse">
       <div class="navbar-brand menu-nav-bar">
-        <a class="navbar-item gpt-logo" href={'#/'}>
-          <img src={logo} alt="ChatGPT-web" width="24" height="24" />
-          <p class="ml-2 is-size-5 has-text-weight-bold">ChatGPT-web</p>
+        <a class="navbar-item gpt-logo">
+          <img src={logo} alt="Chat4ADD" width="24" height="24" />
+          <p class="ml-2 is-size-5 has-text-weight-bold">Chat4ADD</p>
         </a>
         <div class="chat-option-menu navbar-item is-pulled-right">
           <ChatOptionMenu bind:chatId={activeChatId} />
